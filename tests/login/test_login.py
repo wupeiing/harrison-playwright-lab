19abc2e027c83608acef6d57651a00b9fa73f9a7
wupeiing <wupeiing@gmail.com>
2026-04-02 23:25:00 +0800

    chore: handle local time outs.

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class TestLogin:
 
-    def test_signup(self, page):
+    def test_signup_with_valid_information(self, page):
 
         user_info = UserInfo(
             is_male = True,
@@ -37,22 +37,25 @@ class TestLogin:
         home_page = HomePage(page)
         home_page.visit_home_page()
         home_page.click_login_link()
+
         # Ensure New User signeup is there
         assert is_text_on_page(page, "New User Signup!"), "'New User Signup!' should be displayed"
         login_page = LoginPage(page)
         login_page.signup_initial(user_info.name, user_info.email)
+
+        # Ensure 'Enter Account Information' is there after clicking Signup
         assert is_text_on_page(page, "Enter Account Information"), "'Enter Account Information' should be displayed"
         login_page.detail_signup(user_info)
         logger.info("All items are filled, now click continue button")
+
+        # Ensure 'ACCOUNT CREATED!' is there after submitting the form
         assert is_text_on_page(page, "ACCOUNT CREATED!"), "'ACCOUNT CREATED!' should be displayed"
         click_continue(page)
-        time.sleep(10)
-        logger.info("Now here 1")
+
+        # Ensure user is logged in after finish the submitting
         assert is_text_on_page(page, f"Logged in as {user_info.name}"), f"'Logged in as {user_info.name}' should be displayed"
         home_page.click_delete_account_link()
-        time.sleep(10)
-        logger.info("Now here 2")
+
+        # Ensure 'ACCOUNT DELETED!' is there after deleting account
         assert is_text_on_page(page, "ACCOUNT DELETED!"), "'ACCOUNT DELETED!' should be displayed"
         click_continue(page)
-        logger.info("Now here 3")
-        time.sleep(20)
